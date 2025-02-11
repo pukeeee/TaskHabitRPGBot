db5fbf51613ad383cbd2fe480be437b4d018c243
pukeeee <pukee88@proton.me>
2025-02-11 22:17:16 +0200

    none

--- app/core/middlewares/__init__.py
+++
@@ -1,9 +0,0 @@
-from .l10n_middleware import LanguageMiddleware
-from .rate_limit_middleware import RateLimitMiddleware
-from .subscription_middleware import SubscriptionMiddleware
-
-__all__ = [
-    "LanguageMiddleware",
-    "RateLimitMiddleware",
-    "SubscriptionMiddleware"
-]
